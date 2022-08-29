bb512704229f68287419a374f2ed045637166bfd
poovaic <poovai.c@gmail.com>
2022-08-28 23:30:23 -0400

    post image code

--- client/src/components/singlepost/SinglePost.js
+++ client/src/components/singlepost/SinglePost.js
@@ -75,8 +75,10 @@ const handleUpdate= async()=>{
   
     <div className ="singlePost">
       <div className = "singlePostContainer">
-        {post.photo && (
+        {post.photo ? (
       <img className="singlePostImg" src={post.photo} alt=""/>
+      ) :(
+        <img className="singlePostImg" src="https://images.unsplash.com/photo-1565118531796-763e5082d113?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt=""/>
       )}
 
       {/* UPDATING A POST */}
